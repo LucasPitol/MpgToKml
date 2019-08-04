ac84c7422c01c9552cc4c5bba7fd91f2f32044cd
Pitol <lucascpitol0@gmail.com>
2019-08-04 17:14:35 -0300

    Refactor & teclado (incompleto)

--- app/src/main/java/com/pitol/mpgtokml/MainActivity.kt
+++ app/src/main/java/com/pitol/mpgtokml/MainActivity.kt
@@ -9,7 +9,7 @@ import android.widget.TextView
 
 class MainActivity : AppCompatActivity() {
 
-    private val defaultValue: Double = 1.0
+    private val defaultValue: Int = 1
 
     private var mViewHolder: ViewHolder = ViewHolder()
 
@@ -19,25 +19,49 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-
-
         this.mViewHolder.from = findViewById((R.id.from))
         this.mViewHolder.to = findViewById((R.id.to))
         this.mViewHolder.editValue = findViewById(R.id.text_input)
         this.mViewHolder.answer = findViewById(R.id.text_answer)
-        this.mViewHolder.btn = findViewById(R.id.calculate_btn)
+        //this.mViewHolder.btn = findViewById(R.id.calculate_btn)
         this.mViewHolder.switchImg = findViewById(R.id.switch_icon)
 
-        this.mViewHolder.btn.setOnClickListener{
-            calculate()
-        }
+        this.mViewHolder.keyBoard.one = findViewById(R.id.one)
+
+
+        this.mViewHolder.editValue.text = String.format("%d %s", defaultValue, getString(R.string.text_mpg))
 
         this.mViewHolder.switchImg.setOnClickListener{
-            this.mViewHolder = switchCOnversor(this.mViewHolder)
+            this.mViewHolder = switchConversor(this.mViewHolder)
         }
+
+        this.mViewHolder.keyBoard.one.setOnClickListener{
+            addValue(1)
+        }
+
+        calculate()
     }
 
-    private fun switchCOnversor(viewHolder: ViewHolder): ViewHolder{
+    private fun getInput(): Array<String>{
+        return (this.mViewHolder.editValue.text.toString()).split(" ").toTypedArray()
+    }
+
+    private fun addValue(input: Int){
+
+        var editValue = getInput()
+
+        var currentValue: String = editValue[0]
+
+        var unit: String = editValue[1]
+
+        var newValue: String = String.format("%s%d %s", currentValue, input, unit)
+
+        this.mViewHolder.editValue.text = newValue
+
+        calculate()
+    }
+
+    private fun switchConversor(viewHolder: ViewHolder): ViewHolder{
 
         this.mpgToKml = !this.mpgToKml
 
@@ -56,34 +80,55 @@ class MainActivity : AppCompatActivity() {
 
     fun calculate() {
 
-        var value: Double = (this.mViewHolder.editValue.text.toString()).toDouble()
+        var inputValue = (this.mViewHolder.editValue.text.toString()).split(" ").toTypedArray()
+
+        var value: Int = inputValue[0].toInt()
 
         var answer: Double = 1.0
 
-        var unit: String
+        var unitAns: String
+
+        var unitInput: String
 
         if (this.mpgToKml){
 
             answer = (value * 0.425144)
 
-            unit = getString(R.string.text_kml)
+            unitInput = getString(R.string.text_mpg)
+
+            unitAns = getString(R.string.text_kml)
 
         } else {
 
-            answer = (value * 0.354006)
+            answer = (value * 2.35215)
 
-            unit = getString(R.string.text_mpg)
+            unitInput = getString(R.string.text_kml)
+
+            unitAns = getString(R.string.text_mpg)
         }
 
-        this.mViewHolder.answer.text = String.format("%.2f %s", answer, unit)
+        this.mViewHolder.editValue.text = String.format("%d %s", value, unitInput)
+
+        this.mViewHolder.answer.text = String.format("%.2f %s", answer, unitAns)
     }
 
     private class ViewHolder {
+
         lateinit var from: TextView
         lateinit var to: TextView
-        lateinit var editValue: EditText
+        lateinit var editValue: TextView
         lateinit var answer: TextView
         lateinit var btn: Button
         lateinit var switchImg: ImageView
+
+        lateinit var keyBoard: KeyBoard
+
+        init {
+            this.keyBoard = KeyBoard()
+        }
+    }
+
+    private class KeyBoard {
+        lateinit var one: ImageView
     }
 }
